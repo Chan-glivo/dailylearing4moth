4b3944aaa200a89eba20ef75db2b0a2e97750175
Cx-Glivo <1713805014@qq.com>
2020-09-24 15:08:14 +0800

    First test0.1

--- src/classtest3/File_delete.java
+++ src/classtest3/File_delete.java
@@ -10,6 +10,7 @@ public class File_delete {
         File file = new File("." + File.separator + "test.txt");
         if (file.exists()) {
 //            删除指定文件
+
             file.delete();
             System.out.println("该文件已删除！");
         } else {
